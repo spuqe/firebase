d4c1a384b9d250952046a06e804c148a9c607e26
spuqe <CryT0r@protonmail.com>
2020-08-12 23:38:39 +0300

    Update Firebase.py

--- FireLeak/Firebase.py
+++ FireLeak/Firebase.py
@@ -1,19 +1,5 @@
 import requests
-
-print('Please wait while the program is loading...')
-print('')
-print('')
-print('')
-print('')
-print('Just fucking kidding haha.')
-print('Its already running you st00pid.')
-print('')
-print('')
-print('')
-print('')
-print('Well same time this shit script is getting databases to you i will let you read a message from me. You can call me Mr. CryT0r well first of all everyone who will skid this code and claim its theirs should get a life. No one really likes you guys.... And yes. I know im super fucking bad at python just because im too lazy to really learn it.  Thank me later for this little piece of shit that i have made.  Also if ur autistic enough to now know how to use DONT FUCKING ASK HELP FROM ME! well this is the end of this shit text! Have fun time checking those databases when the script is done checking! also remember to follow my tw33ter! https://twitter.com/T0rCry also right now im wondering... How fucking cool and not so boring this text would look like if it had an color? Well anyways have fun time leaking shit! I wish you good luck on life and shit you know? Im trying to make you like me <3 well ok ok this was enough right? Well see you next time!')
-print('Right now this script is loading! Dont fucking touch it or it will crash. Ofc you can always exit from it by doing CTRL + C on linux or just by closing the application')
-
+print('Have fun')
 
 # Btw i fucking hate python but i wanted to try what i could create with it :) so be fucking thankful for that! 
 
